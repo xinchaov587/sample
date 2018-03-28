78163dda1110df80d73bfd77680e7b5e15c5752b
xinchao <xinchaov587@gmail.com>
2018-03-28 13:11:34 +0000

    注册页面

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 // 首页渲染
-Route::get('/', 'StaticPagesController@home');
+Route::get('/', 'StaticPagesController@home')->name('home');
 // 帮助页渲染
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/help', 'StaticPagesController@help')->name('help');
 // 关于页渲染
-Route::get('/about', 'StaticPagesController@about');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+// 用户注册
+Route::get('signup', 'UsersController@create')->name('signup');
