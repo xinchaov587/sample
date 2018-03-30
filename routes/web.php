f251039960006b31e29f419a6b3fce5a92fb7544
xinchao <xinchaov587@gmail.com>
2018-03-30 08:09:11 +0000

    粉丝

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,9 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 // 微博创建 和 删除
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
